2ba901ec49f32e438a150927646a62fcd57dce86
HoangThiThaoNhi <119788935+HoangThiThaoNhi@users.noreply.github.com>
2025-03-23 01:36:29 +0700

    Update LoginScreen

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:getting_started/app.dart';
 import 'package:provider/provider.dart';
 import 'presentation/widgets/bottom_nav.dart';
+import 'presentation/screens/auth/login_screen.dart';
+
+import 'package:getting_started/presentation/screens/auth/login_screen.dart';
 
 void main() {
   runApp(const ComicApp());
+}
+
+class ComicApp extends StatelessWidget {
+  const ComicApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: LoginScreen());
+  }
 }
 
 // class MyApp extends StatelessWidget {
--- lib/presentation/screens/auth/login_screen.dart
+++ lib/presentation/screens/auth/login_screen.dart
@@ -1,15 +1,161 @@
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key});
-
-  @override
-  State<LoginScreen> createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
+class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text("Login Screen"));
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Welcome",
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 5),
+              const Text(
+                "Sign in to start",
+                style: TextStyle(fontSize: 16, color: Colors.grey),
+              ),
+              const SizedBox(height: 30),
+
+              // Nút đăng nhập Google
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withAlpha(76),
+                      spreadRadius: 3,
+                      blurRadius: 7,
+                      offset: Offset(0, 4),
+                    ),
+                  ],
+                ),
+                child: ElevatedButton.icon(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    foregroundColor: Colors.black,
+                    minimumSize: const Size(double.infinity, 50),
+                  ),
+                  onPressed: () {},
+                  icon: Image.asset('assets/google_logo.png', height: 24),
+                  label: const Text("Continue with Google"),
+                ),
+              ),
+              const SizedBox(height: 15),
+
+              // Nút đăng nhập Metar
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withAlpha(76),
+                      spreadRadius: 3,
+                      blurRadius: 7,
+                      offset: Offset(0, 4),
+                    ),
+                  ],
+                ),
+                child: ElevatedButton.icon(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    foregroundColor: Colors.white,
+                    minimumSize: const Size(double.infinity, 50),
+                  ),
+                  onPressed: () {},
+                  icon: Image.asset('assets/meta_logo.png', height: 24),
+                  label: const Text("Continue with Meta"),
+                ),
+              ),
+
+              const SizedBox(height: 20),
+              TextButton(
+                onPressed: () {},
+                child: const Text.rich(
+                  TextSpan(
+                    text: "Haven't account? ",
+                    style: TextStyle(color: Colors.black),
+                    children: [
+                      TextSpan(
+                        text: "Sign up!",
+                        style: TextStyle(color: Colors.blue),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 20),
+
+              // Form đăng nhập
+              Container(
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  gradient: const LinearGradient(
+                    colors: [Color(0xFFFFF1BE), Color(0xFFA2B2FC)],
+                    begin: Alignment(-0.9, -0.9),
+                    end: Alignment(0.9, 0.9),
+                  ),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Column(
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: "Login",
+                        border: UnderlineInputBorder(),
+                        labelStyle: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    TextField(
+                      obscureText: true,
+                      style: TextStyle(
+                        color: Colors.white,
+                      ), // Đổi màu chữ khi nhập thành trắng
+                      decoration: InputDecoration(
+                        labelText: "Password",
+                        border: UnderlineInputBorder(),
+                        labelStyle: TextStyle(
+                          color: Colors.white,
+                        ), // Đổi màu nhãn thành trắng
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                        onPressed: () {},
+                        child: const Text(
+                          "Forgot password?",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+
+                    // Nút continue
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.grey,
+                        foregroundColor: Colors.white,
+                        minimumSize: const Size(double.infinity, 50),
+                      ),
+                      onPressed: () {},
+                      child: const Text("Continue"),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
